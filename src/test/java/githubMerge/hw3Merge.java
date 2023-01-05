76d468bfc066adc4bf474ce4ce6089758e3eb23d
Taygib <nfsmwm5@mail.ru>
2023-01-05 16:33:12 +0300

    Test added

--- src/test/java/githubMerge/hw3Merge.java
+++ src/test/java/githubMerge/hw3Merge.java
@@ -1,21 +1,20 @@
 package githubMerge;
 
+import org.junit.jupiter.api.Test;
+
 public class hw3Merge {
 
-    public static void main (String args[]) {
+    @Test
+    void pullRequest () {
 
         int a = 5;
         int b = 20;
 
-        int c = a + b%3;
+        int c = a + b - 18;
 
       // c = 7
 
         System.out.println(c);
-
-
-
-
 
     }
 
