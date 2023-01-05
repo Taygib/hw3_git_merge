3d574caf230f8f41beddb6071a259b2f0b4889ac
Taygib <nfsmwm5@mail.ru>
2023-01-05 15:53:44 +0300

    init commit

---
+++ src/test/java/githubMerge/hw3Merge.java
@@ -0,0 +1,23 @@
+package githubMerge;
+
+public class hw3Merge {
+
+    public static void main (String args[]) {
+
+        int a = 5;
+        int b = 20;
+
+        int c = a + b%3;
+
+      // c = 7
+
+        System.out.println(c);
+
+
+
+
+
+    }
+
+
+}
